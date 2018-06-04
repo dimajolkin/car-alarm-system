2705a9a9a1c61a7731de8800a31849e4a8e8fa82
dimajolkin <dimajolkin@rambler.ru>
2018-06-04 20:16:29 +0300

    Addflash for antenna

---
+++ antenna/antenna.ino
@@ -0,0 +1,38 @@
+
+#define D1 2
+#define D2 3
+#define D3 4
+#define D4 5
+
+#define DELAY 400
+
+void setup () {
+   Serial.begin(9600);
+    pinMode(D1, INPUT);
+    pinMode(D2, INPUT);
+    pinMode(D3, INPUT);
+    pinMode(D4, INPUT);
+}
+
+void loop() {
+     
+  if (digitalRead(D1) == HIGH) {
+      Serial.write('d');
+      delay(DELAY);
+  }
+
+  if (digitalRead(D2) == HIGH) {
+     Serial.write('c');
+      delay(DELAY);
+  }
+
+  if (digitalRead(D3) == HIGH) {
+       Serial.write('b');
+      delay(DELAY);
+  }
+  
+  if (digitalRead(D4) == HIGH) {
+       Serial.write('a');
+      delay(DELAY);
+  }
+}
---
+++ antenna/base/base.ino
@@ -0,0 +1,17 @@
+int incomingByte = 0;   // для хранения полученного байта
+ 
+void setup() {
+    Serial.begin(9600); // устанавливаем последовательное соединение
+}
+ 
+void loop() {
+    if (Serial.available() > 0) {  //если есть доступные данные
+        // считываем байт
+        incomingByte = Serial.read();
+ 
+        // отсылаем то, что получили
+        Serial.print("I received: ");
+        Serial.println(incomingByte, DEC);
+    }
+}
+
